ef0bdd942ddeba304e1ac7b5578d6e019d9c3893
Sergio <sreina@unal.edu.co>
2021-08-28 11:48:48 -0500

    navabr fixed

--- index.js
+++ index.js
@@ -12,3 +12,41 @@ document.addEventListener('mouseup', (event) => {
     if (!menuBtn.contains(event.target)) navMenu.style.display = 'none'
   }
 })
+
+//Set date on footer
+const date = document.querySelector('.date')
+date.innerHTML = new Date().getFullYear()
+
+const navbar = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset
+  const navHeight = navbar.getBoundingClientRect().height
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-header')
+  } else {
+    navbar.classList.remove('fixed-header')
+  }
+})
+
+const navLinks = document.querySelectorAll('.nav-link:not(.home)')
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = e.currentTarget.getAttribute('href').slice(2)
+    const element = document.getElementById(id)
+
+    const navHeight = navbar.getBoundingClientRect().height
+    const fixedNav = navbar.classList.contains('fixed-header')
+    let position = element.offsetTop - navHeight
+
+    if (!fixedNav) position = position - navHeight
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    })
+  })
+})
